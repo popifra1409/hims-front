83dd49a547800995398ad6540f6bf84edb7da5bf
Djieuga Djoubi <djieugadjoubirylenathan@gmail.com>
2023-01-30 16:40:24 +0100

    Création du composant: Téléchargement de fichiers du formulaire d'identification du patient

--- src/views/dashboard/components/patient/PatientImgUpload.js
+++ src/views/dashboard/components/patient/PatientImgUpload.js
@@ -1,51 +1,24 @@
+import { BsCloudArrowUp } from 'react-icons/bs'
+import { useRef } from 'react'
 
 const PatientImgUpload = () => {
+  const inputRef = useRef(null)
+
+  const uploadFile = (e) => {
+    inputRef.current = e.target.files[0].name
+  }
+
   return (
     <>
-      <div className="row m-3 p-2">
-        <label className="col-3" for="name">Tiers-payant </label>
-        <select className="col-9 w-full" type="text" name="tiersPayant">
-          <option value=""></option>
-          <option value="payant1">payant1</option>
-          <option value="payant2">payant2</option>
-        </select>
-      </div>
-      <div className="row m-3 p-1">
-        <div className="col-md-6">
-          <div className="row">
-            <label className="col-6" for="name">TM%</label>
-            <select className="col-6" type="text" name="TM">
-              <option value=""></option>
-              <option value="TM1">TM1</option>
-              <option value="TM2">TM2</option>
-            </select>
-          </div>
+      <label for="file" className='p-0 m-5 col border-primary rounded' style={{ border: '2px dotted', cursor: 'pointer' }} title="Clickez" >
+        <div className='p-2 m-5'>
+          <BsCloudArrowUp style={{ width: '100px', height: '100px' }} />
+          <br />
+          Clickez ici pour ajouter un fichier
+          <br />
+          <input ref={inputRef} type="file" name="file" id='file' onChange={uploadFile} style={{ display: 'none' }} />
         </div>
-        <div className="col-md-6">
-          <div className="row">
-            <label className="col-5" for="name">Matricule patient </label>
-            <input className="col-7" type="text" name="matriculePatient" />
-          </div>
-        </div>
-      </div>
-      <div className="row ">
-        <label className="col-3 " for="name">Contrat Patient </label>
-        <input className="col-8 ms-3" type="number" name="contratPatient" />
-      </div>
-      <div className="row m-4">
-        <label className="col-5" for="name">Prise en charge perso</label>
-        <input className="col-1 " type="checkbox" name="PriseEnchargePerso" />
-      </div>
-      <div className="col-md-6">
-        <div className="row">
-          <label className="col-6" for="name">Société</label>
-          <select className="col-5 ms-3" type="text" name="company">
-            <option value=""></option>
-            <option value="TM1">TM1</option>
-            <option value="TM2">TM2</option>
-          </select>
-        </div>
-      </div>
+      </label>
     </>
   )
 }
