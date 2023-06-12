2391d1a6576c7b05d2ca34b3d3ba14410f45299b
loichounang <hounangloic21@gmail.com>
2023-06-12 22:05:39 +0100

    Ajout de la fonctionnalité de survol aux éléments de la barre latérale et qui affichage des sous-éléments lorsque la souris passe sur un élément spécifique

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -21,10 +21,72 @@ import SyncAltIcon from '@mui/icons-material/SyncAlt';
 import LocalHotelIcon from '@mui/icons-material/LocalHotel';
 import { Link } from "react-router-dom";
 import { DarkModeContext } from "../../context/darkModeContext";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 const Sidebar = () => {
   const { dispatch } = useContext(DarkModeContext);
+
+  {/* opération pour le hover de l'accueil */ }
+  const [accueil, setAccueil] = useState(false);
+
+  const handleMouseAccueilEnter = () => {
+    setAccueil(true);
+  };
+
+  const handleMouseAccueilLeave = () => {
+    setAccueil(false);
+  };
+
+  {/* opération pour le hover d'admission'*/ }
+
+  const [admission, setAdmission] = useState(false);
+
+  const handleMouseAdmissionEnter = () => {
+    setAdmission(true);
+  };
+
+  const handleMouseAdmissionLeave = () => {
+    setAdmission(false);
+  };
+
+
+  {/* opération pour le hover de la pharmacie*/ }
+
+  const [pharmacie, setPharmacie] = useState(false);
+
+  const handleMousePharmacieEnter = () => {
+    setPharmacie(true);
+  };
+
+  const handleMousePharmacieLeave = () => {
+    setPharmacie(false);
+  };
+
+  {/* opération pour le hover de parametrage */ }
+
+  const [parametrage, setParametrage] = useState(false);
+
+  const handleMouseParametrageEnter = () => {
+    setParametrage(true);
+  };
+
+  const handleMouseParametrageLeave = () => {
+    setParametrage(false);
+  };
+
+  {/* opération pour le hover des utilisateurs */ }
+
+  const [utilisateurs, setUtilisateurs] = useState(false);
+
+  const handleMouseUtilisateursEnter = () => {
+    setUtilisateurs(true);
+  };
+
+  const handleMouseUtilisateursLeave = () => {
+    setUtilisateurs(false);
+  };
+
+
   return (
     <div className="sidebar">
       <div className="top">
@@ -35,6 +97,7 @@ const Sidebar = () => {
       <hr />
       <div className="center">
         <ul>
+
           <p className="title">MENU PRINCIPAL</p>
           <Link to="/" style={{ textDecoration: "none" }}>
             <li>
@@ -42,118 +105,179 @@ const Sidebar = () => {
               <span>Tableau de bord</span>
             </li>
           </Link>
-          <p className="title">Accueil</p>
-          <Link to="/identification/patients" style={{ textDecoration: "none" }}>
-            <li>
-              < PersonOutlineIcon className="icon" />
-              <span>Patient</span>
-            </li>
-          </Link>
-          <Link to="/identification/groupePatients" style={{ textDecoration: "none" }}>
-            <li>
-              <GroupOutlinedIcon className="icon" />
-              <span>Groupe patient</span>
-            </li>
-          </Link>
-          <Link to="/products" style={{ textDecoration: "none" }}>
-            <li>
-              <MedicalServicesIcon className="icon" />
-              <span>Unités fonctionnelles</span>
-            </li>
-          </Link>
-          <Link to="/products" style={{ textDecoration: "none" }}>
-            <li>
-              <TextSnippetIcon className="icon" />
-              <span>Documents</span>
-            </li>
-          </Link>
 
-          <p className="title">Admission</p>
-          <Link to="/identification/patients" style={{ textDecoration: "none" }}>
-            <li>
-              < LocalHotelIcon className="icon" />
-              <span>Hospitalisation</span>
-            </li>
-          </Link>
-          <Link to="/products" style={{ textDecoration: "none" }}>
-            <li>
-              < LocalHospitalIcon className="icon" />
-              <span>Urgence</span>
-            </li>
-          </Link>
-          <Link to="/products" style={{ textDecoration: "none" }}>
-            <li>
-              <AirlineSeatFlatIcon className="icon" />
-              <span>Morgue</span>
-            </li>
-          </Link>
+          {/* opération pour le hover de l'accueil */}
+          <div onMouseEnter={handleMouseAccueilEnter}
+            onMouseLeave={handleMouseAccueilLeave}
+          >
+            <p className="title">Accueil</p>
+            {accueil && (
+              <>
+                <Link to="/identification/patients" style={{ textDecoration: "none" }}>
+                  <li>
+                    < PersonOutlineIcon className="icon" />
+                    <span>Patient</span>
+                  </li>
+                </Link>
+                <Link to="/identification/groupePatients" style={{ textDecoration: "none" }}>
+                  <li>
+                    <GroupOutlinedIcon className="icon" />
+                    <span>Groupe patient</span>
+                  </li>
+                </Link>
+                <Link to="/products" style={{ textDecoration: "none" }}>
+                  <li>
+                    <MedicalServicesIcon className="icon" />
+                    <span>Unités fonctionnelles</span>
+                  </li>
+                </Link>
 
-          <p className="title">Pharmacie</p>
-          <Link to="/identification/patients" style={{ textDecoration: "none" }}>
-            <li>
-              < MedicationIcon className="icon" />
-              <span>Produit</span>
-            </li>
-          </Link>
-          <Link to="/products" style={{ textDecoration: "none" }}>
-            <li>
-              <SettingsIcon className="icon" />
-              <span>Paramétrage</span>
-            </li>
-          </Link>
-          <Link to="/products" style={{ textDecoration: "none" }}>
-            <li>
-              <SyncAltIcon className="icon" />
-              <span>Mouvement interne</span>
-            </li>
-          </Link>
-          <Link to="/products" style={{ textDecoration: "none" }}>
-            <li>
-              <KeyboardBackspaceIcon className="icon" />
-              <span>Mouvement externe</span>
-            </li>
-          </Link>
-          <Link to="/products" style={{ textDecoration: "none" }}>
-            <li>
-              <AddBusinessIcon className="icon" />
-              <span>Mouvement stock</span>
-            </li>
-          </Link>
-          <Link to="/products" style={{ textDecoration: "none" }}>
-            <li>
-              <InventoryIcon className="icon" />
-              <span>Inventaire</span>
-            </li>
-          </Link>
-          <Link to="/products" style={{ textDecoration: "none" }}>
-            <li>
-              <AddAlertIcon className="icon" />
-              <span>Alerte</span>
-            </li>
-          </Link>
+                <Link to="/products" style={{ textDecoration: "none" }}>
+                  <li>
+                    <TextSnippetIcon className="icon" />
+                    <span>Documents</span>
+                  </li>
+                </Link>
+              </>
+            )}
+          </div>
 
-          <p className="title">Paramétrage</p>
-          <li>
-            <LocalHospitalOutlinedIcon className="icon" />
-            <span>Etablissement</span>
-          </li>
-          <li>
-            <PsychologyOutlinedIcon className="icon" />
-            <span>Logs</span>
-          </li>
-          <li>
-            <SettingsApplicationsIcon className="icon" />
-            <span>Settings</span>
-          </li>
-          <p className="title">Utilisateurs</p>
-          <li>
-            <AccountCircleOutlinedIcon className="icon" />
-            <span>Profile</span>
-          </li>
-          <li>
-            <ExitToAppIcon className="icon" />
-            <span>Logout</span>
-          </li>
+          {/* opération pour le hover d'admission */}
+          <div onMouseEnter={handleMouseAdmissionEnter}
+            onMouseLeave={handleMouseAdmissionLeave}>
+
+            <p className="title">Admission</p>
+            {admission && (
+              <>
+
+                <Link to="/identification/patients" style={{ textDecoration: "none" }}>
+                  <li>
+                    < LocalHotelIcon className="icon" />
+                    <span>Hospitalisation</span>
+                  </li>
+                </Link>
+                <Link to="/products" style={{ textDecoration: "none" }}>
+                  <li>
+                    < LocalHospitalIcon className="icon" />
+                    <span>Urgence</span>
+                  </li>
+                </Link>
+                <Link to="/products" style={{ textDecoration: "none" }}>
+                  <li>
+                    <AirlineSeatFlatIcon className="icon" />
+                    <span>Morgue</span>
+                  </li>
+                </Link>
+              </>
+            )}
+          </div>
+
+          {/* opération pour le hover de la pharmacie */}
+          <div onMouseEnter={handleMousePharmacieEnter}
+            onMouseLeave={handleMousePharmacieLeave}
+          >
+            <p className="title">Pharmacie</p>
+            {pharmacie && (
+              <>
+                <Link to="/identification/patients" style={{ textDecoration: "none" }}>
+                  <li>
+                    < MedicationIcon className="icon" />
+                    <span>Produit</span>
+                  </li>
+                </Link>
+                <Link to="/products" style={{ textDecoration: "none" }}>
+                  <li>
+                    <SettingsIcon className="icon" />
+                    <span>Paramétrage</span>
+                  </li>
+                </Link>
+                <Link to="/products" style={{ textDecoration: "none" }}>
+                  <li>
+                    <SyncAltIcon className="icon" />
+                    <span>Mouvement interne</span>
+                  </li>
+                </Link>
+                <Link to="/products" style={{ textDecoration: "none" }}>
+                  <li>
+                    <KeyboardBackspaceIcon className="icon" />
+                    <span>Mouvement externe</span>
+                  </li>
+                </Link>
+                <Link to="/products" style={{ textDecoration: "none" }}>
+                  <li>
+                    <AddBusinessIcon className="icon" />
+                    <span>Mouvement stock</span>
+                  </li>
+                </Link>
+                <Link to="/products" style={{ textDecoration: "none" }}>
+                  <li>
+                    <InventoryIcon className="icon" />
+                    <span>Inventaire</span>
+                  </li>
+                </Link>
+                <Link to="/products" style={{ textDecoration: "none" }}>
+                  <li>
+                    <AddAlertIcon className="icon" />
+                    <span>Alerte</span>
+                  </li>
+                </Link>
+              </>
+            )}
+          </div>
+
+          {/* opération pour le hover du parametrage */}
+          <div
+            onMouseEnter={handleMouseParametrageEnter}
+            onMouseLeave={handleMouseParametrageLeave}
+          >
+            <p className="title">Paramétrage</p>
+            {parametrage && (
+              <>
+                <Link to="/identification/patients" style={{ textDecoration: "none" }}>
+                  <li>
+                    <LocalHospitalOutlinedIcon className="icon" />
+                    <span>Etablissement</span>
+                  </li>
+                </Link>
+                <Link to="/identification/patients" style={{ textDecoration: "none" }}>
+                  <li>
+                    <PsychologyOutlinedIcon className="icon" />
+                    <span>Logs</span>
+                  </li>
+                </Link>
+                <Link to="/identification/patients" style={{ textDecoration: "none" }}>
+                  <li>
+                    <SettingsApplicationsIcon className="icon" />
+                    <span>Settings</span>
+                  </li>
+                </Link>
+              </>
+            )}
+          </div>
+
+          {/* opération pour le hover des utilisateurs */}
+          <div
+            onMouseEnter={handleMouseUtilisateursEnter}
+            onMouseLeave={handleMouseUtilisateursLeave}
+          >
+            <p className="title">Utilisateurs</p>
+            {utilisateurs && (
+              <>
+                <Link to="/identification/patients" style={{ textDecoration: "none" }}>
+                  <li>
+                    <AccountCircleOutlinedIcon className="icon" />
+                    <span>Profile</span>
+                  </li>
+                </Link>
+                <Link to="/identification/patients" style={{ textDecoration: "none" }}>
+                  <li>
+                    <ExitToAppIcon className="icon" />
+                    <span>Logout</span>
+                  </li>
+                </Link>
+              </>
+            )}
+          </div>
         </ul>
       </div>
       <div className="bottom">
